11224886d3743bd7ad2952eb5bf1ad13ce571df7
Th3Lourde <th3sylvia.lourde@gmail.com>
2018-02-05 18:26:14 -0800

    Desktop-Home-Done

--- js/master.js
+++ js/master.js
@@ -4,10 +4,12 @@
 //     alert("Animate Up");
 //     $("#black-menu").animate({height: '10vw'}, 5, 'in-out')
 // });
+// Makes the menu move when clicked
+// Toggle for simplicity
 // var animateVar = true
 document.getElementById("animateUp").addEventListener("click", function() {
-    var elementToSlide = document.getElementById("mymenu");
-    document.getElementById("black-menu").classList.toggle("is-active");
+    // var elementToSlide = document.getElementById("mymenu");
     $( "#mymenu" ).fadeToggle( "slow", "linear" );
+    document.getElementById("black-menu").classList.toggle("is-active");
     // $('li').style
 });
